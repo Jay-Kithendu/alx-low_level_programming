91baec2da67737c52cdc146cf0d263c417a1228d
Jay0622 <james98katiwa@gmail.com>
2022-07-01 07:36:51 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - program Entry point
  *
- * Description: A program to determine whether an int is positive, negative or zero
+ * Description: A program to determine type of int
  *
  * Return: 0 indicates success
  */
@@ -14,14 +14,14 @@ int main(void)
 {
 
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if(n>0)
+	if (n > 0)
 	{
-		printf("%d is positive\n", n);	
+		printf("%d is positive\n", n);
 	}
-	else if(n==0)
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
