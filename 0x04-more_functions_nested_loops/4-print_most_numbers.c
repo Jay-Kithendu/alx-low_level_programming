96a151ac940457f69803599fbe6a3f52e5627dd4
Jay0622 <james98katiwa@gmail.com>
2022-07-07 07:39:31 -0700

    function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,13 @@ void print_most_numbers(void)
 {
 	int x = 0;
 
-	while ((x <= 9) && (x != 2 && x != 4))
-	{	_putchar(x);
+	while (x <= 9)
+	{
+		if (x == 2 || x == 4)
+			continue;
+		_putchar(x);
 		x++;
+	
 	}
 	_putchar('\n');
 }
