0f6dbaef616135821777569d7d130f2782d1f3ba
Jay0622 <james98katiwa@gmail.com>
2022-07-14 08:34:59 -0700

    function  that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,8 +14,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && str[i] != '\0'; i++)
-		dest[i] = src [i];
-	for (; i< n; i++)
+		dest[i] = sr [i];
+	for (; i < n; i++)
 		dest[i] = '\0';
 
 	return (dest);
