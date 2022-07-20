48d538b43859e6490c59cb8528cb2298574dc6b7
Jay0622 <james98katiwa@gmail.com>
2022-07-20 09:54:15 -0700

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,26 @@
 #include "main.h"
-
 /**
- * _this_is_prime - recurcively if a number is prime
- * @n: integer n
- * @_is_divisor: check if is a divisor
- *
- * Return: SUCCESS ? 1 : 0
- */
-
-int _this_is_prime(int n, int _is_divisor)
+   * primenumber - checks if a given number is prime
+    * @n: given number
+     * @m: checker number
+      * Return: 1 if number is prime else 0
+       **/
+int primenumber(int n, int m)
 {
-	if (n == _is_divisor)
-		return (TRUE);
-
-	if (n % _is_divisor == NULL)
-		return (NULL);
-
-	return (_this_is_prime(n, _is_divisor + 1));
+		if (m == 1)
+					return (1);
+			if (n % m == 0)
+						return (0);
+				return (primenumber(n, m - 1));
 }
-
 /**
- * is_prime_number - call @_this_is_prime func
- * @n: integer n
- *
- * Return: SUCCESS ? 1 : 0
- */
-
+   * is_prime_number - checks if a given number is prime
+    * @n: given number
+     * Return: 1 if number is prime else 0
+      **/
 int is_prime_number(int n)
 {
-	if ((n % 2 == NULL && n != 2) || n < 2)
-		return (NULL);
-	
-	return (_this_is_prime(n, 2));
+		if (n <= 1)
+					return (0);
+			return (primenumber(n, n / 2));
 }
