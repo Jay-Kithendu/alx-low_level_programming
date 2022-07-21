f282a6c8c7bca7e6e9073d4ea6a9fc177600ba1d
Jay0622 <james98katiwa@gmail.com>
2022-07-20 18:21:35 -0700

    betty corrections

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,30 +10,22 @@
 
 int substring_match(char *s1, char *s2, char *after_wldcd)
 {
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
-		if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
 
-					return (1);
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
 
-			if (*s1 == '\0' && *s2 == '*')
+	if (*s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
 
-						return (substring_match(s1, s2 + 1, s2 + 1));
-
-				if (*s1 == '\0' && *s2 != '\0')
-
-							return (0);
-
-					if (*s2 == '*')
-
-								return (substring_match(s1, s2 + 1, s2 + 1));
-
-						if (*s1 == *s2)
-
-									return (substring_match(s1 + 1, s2 + 1, after_wldcd));
-
-							else
-
-										return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
 
 }
 
@@ -46,20 +38,15 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 
 int wildcmp(char *s1, char *s2)
 {
-		if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
 
-					return (1);
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
 
-			if (*s1 == *s2)
-
-						return (wildcmp(s1 + 1, s2 + 1));
-
-				else if (*s2 == '*')
-
-							return (substring_match(s1, (s2 + 1), (s2 + 1)));
-
-					else
-
-								return (0);
+	else
+		return (0);
 
 }
