27cc22bafe4b5fd58b65c3870a4e7259e6879cb1
Jay0622 <james98katiwa@gmail.com>
2022-07-25 04:34:43 -0700

    program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,46 +1,43 @@
 #include "main.h"
 
 /**
-  * isInteger - check if its an int
-  * @s: string to check
-  *
-  * Return: 0 or 1
-  */
-
-int isInteger(const char *s)
+    * main - Prints the sum of args positive numbers
+      * @argc: argument count
+        * @argv: argument vector
+	  *
+	    * Return: Always zero
+	      */
+int main(int argc, char *argv[])
 {
-	int i = 0;
+		int i;
+			unsigned int k, sum = 0;
+				char *e;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
-}
+					if (argc > 1)
+							{
+										for (i = 1; i < argc; i++)
+													{
+																	e = argv[i];
 
-/**
-  * main -  program that adds positive numbers
-  * @argc: counter
-  * @argv: vector
-  *
-  * Return: Always 0;
-  */
+																				for (k = 0; k < strlen(e); k++)
+																								{
+																													if (e[k] < 48 || e[k] > 57)
+																																		{
+																																								printf("Error\n");
+																																													return (1);
+																																																	}
+																																}
 
-int main(int argc, char const *argv[])
-{
-	int sum = 0;
+																							sum += atoi(e);
+																										e++;
+																												}
 
-	while (--argc)
-	{
-		if (isInteger(argv[argc]))
-		{
-			print("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[argc]);
-	}
-	printf("%i\n", sum);
-	return (0);
+												printf("%d\n", sum);
+													}
+						else
+								{
+											printf("0\n");
+												}
+
+							return (0);
 }
