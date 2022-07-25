e5664356aee1ec72c07339bb0bb9a2d60acabd68
Jay0622 <james98katiwa@gmail.com>
2022-07-25 03:19:50 -0700

    Write a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+  * isInt - check if its an int
+  * @s: string to check
+  *
+  * Return: 0 or 1
+  */
+
+int isInt(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+  * main -  program that adds positive numbers
+  * @argc: counter
+  * @argv: vector
+  *
+  * Return: Always 0;
+  */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	while (--argc)
+	{
+		if (isInt(argv[argc]))
+		{
+			print("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
