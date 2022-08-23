59f11f1abca8e01cfbca3e6ba341f85732a5afe8
Jay0622 <james98katiwa@gmail.com>
2022-08-23 05:06:32 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,28 +2,26 @@
 
 /**
    * main - Entry point
-    * @argc: num of args
-     * @argv: array of args
-      *
-       * Return: Always 0 (Success)
-        */
+   * @argc: num of args
+   * @argv: array of args
+   *
+   * Return: Always 0 (Success)
+   */
 
 int main(int argc, char *argv[])
 {
-		int a, b;
-			int (*f)(int, int);
+	int a, b;
+	int (*f)(int, int);
 
-				if (argc != 4)
-						{
-									ERR_OUT;
-											ERR_NUM;
-												}
+	if (argc != 4)
+	{
+		ERR_OUT;
+		ERR_NUM;
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
-					a = atoi(argv[1]);
-						b = atoi(argv[3]);
-							f = get_op_func(argv[2]);
-
-								printf("%d\n", f(a, b));
-
-									return (0);
+	printf("%d\n", f(a, b));
+	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,71 +2,71 @@
 
 /**
    * op_add - addition
-    * @a: value
-     * @b: value
-      *
-       * Return: integer
-        */
+   * @a: value
+   * @b: value
+   *
+   * Return: integer
+   */
 int op_add(int a, int b)
 {
-		return (a + b);
+	return (a + b);
 }
 
 /**
    * op_sub - subtraction
-    * @a: value
-     * @b: value
-      *
-       * Return: integer
-        */
+   * @a: value
+   * @b: value
+   *
+   * Return: integer
+   */
 int op_sub(int a, int b)
 {
-		return (a - b);
+	return (a - b);
 }
 
 /**
    * op_mul - multiplication
-    * @a: value
-     * @b: value
-      *
-       * Return: integer
-        */
+   * @a: value
+   * @b: value
+   *
+   * Return: integer
+   */
 int op_mul(int a, int b)
 {
-		return (a * b);
+	return (a * b);
 }
 
 /**
    * op_div - division
-    * @a: value
-     * @b: value
-      *
-       * Return: integer
-        */
+   * @a: value
+   * @b: value
+   *
+   * Return: integer
+   */
 int op_div(int a, int b)
 {
-		if (b == 0)
-				{
-							ERR_OUT;
-									ERR_INF;
-										}
-			return (a / b);
+	if (b == 0)
+	{
+		ERR_OUT;
+		ERR_INF;
+	}
+	return (a / b);
 }
 
 /**
    * op_mod - modulus
     * @a: value
-     * @b: value
-      *
-       * Return: integer
-        */
+    * @b: value
+    *
+    * Return: integer
+    */
 int op_mod(int a, int b)
 {
-		if (b == 0)
-				{
-							ERR_OUT;
-									ERR_INF;
-										}
+	if (b == 0)
+	{
+		ERR_OUT;
+		ERR_INF;
+	}
 
-			return (a % b);
+	return (a % b);
 }
